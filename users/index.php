a6d65a4e843e033fe57e690f27d5366fe968ca1f
Nicodemus Opon <nicodemusopon@gmail.com>
2020-09-06 18:02:52 +0300

    2020-09-06T18:02

--- users/index.php
+++ users/index.php
@@ -1,11 +1,11 @@
 <?php
-//header('Content-type: application/json');
+header('Content-type: application/json');
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 
-echo(json_encode($_POST, JSON_UNESCAPED_SLASHES));
+echo(json_encode($_GET, JSON_UNESCAPED_SLASHES));
 
 ?>
