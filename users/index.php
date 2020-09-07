b18fc6939556eb60ce54146b9b4cebcc1686e748
Nicodemus Opon <nicodemusopon@gmail.com>
2020-09-07 17:23:39 +0300

    2020-09-07T17:23

--- users/index.php
+++ users/index.php
@@ -1,11 +1,60 @@
 <?php
 header('Content-type: application/json');
 
+include '../include/connect.php';
+include '../include/auth.php';
+include '../include/methods_.php';
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+$datax = json_decode(file_get_contents('php://input'), true);
 
-echo(json_encode($_POST, JSON_UNESCAPED_SLASHES));
+//echo(json_encode($datax, JSON_UNESCAPED_SLASHES));
+//print_r($datax);
+$ausername = $datax["user_name"];
+$apassword = $datax["pass_word"];
+$aemail = $datax["email_address"];
+
+
+$stmt = $con->prepare("INSERT INTO user (username, email, password,name,profile_pic,following) VALUES (?, ?, ?,?,?,?)");
+$blnk = "_";
+$blnkno = "0";
+$stmt->bind_param("ssssss", $ausername, $aemail, $apassword, $blnk, $blnk, $blnkno);
+
+
+//$stmt->bind_param("i", $id);
+$stmt->execute();
+
+//echo "New records created successfullyn ";
+//echo $con->error;
+
+//$stmt->close();
+if (isset($_GET["auth"])) {
+    $auth_k = $_GET["auth"];
+    if (auth($auth_k) != "invalid") {
+        if (isset($_GET["q"])) {
+            $sqql = "SELECT * FROM users where username='" . $_GET["q"] . "'";;
+        } else {
+            $sqql = "SELECT * FROM users";
+        }
+
+        $result = $con->query($sqql);
+        while ($row = $result->fetch_assoc()) {
+            $rows[] = $row;
+
+        }
+
+
+        $formatted_results = $rows;
+        print display_json($formatted_results);
+
+    } else {
+        echo display_json("Invalid API Key");
+    }
+} else {
+    echo display_json("Missing API Key");
+}
 
 ?>
--- users/test.php
+++ users/test.php
@@ -3,3 +3,11 @@ $datax = json_decode(file_get_contents('php://input'), true);
 
 echo(json_encode($datax, JSON_UNESCAPED_SLASHES));
 
+$ausername = $datax["user_name"];
+$apassword = $datax["pass_word"];
+$aemail = $datax["email_address"];
+
+// prepare and bind
+$stmt = $con->prepare("INSERT INTO MyGuests (firstname, lastname, email) VALUES (?, ?, ?)");
+$stmt->bind_param("sss", $firstname, $lastname, $email);
+
