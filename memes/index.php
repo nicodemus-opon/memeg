3deffd5ba4b54026e29934724fc98bf77220549f
Nicodemus Opon <nicodemusopon@gmail.com>
2020-09-10 12:24:37 +0300

    2020-09-10T12:24

--- memes/index.php
+++ memes/index.php
@@ -8,7 +8,7 @@ if (isset($_GET["auth"])) {
     $auth_k = $_GET["auth"];
     if (auth($auth_k) != "invalid") {
         if (isset($_GET["q"])) {
-            $sqql = "SELECT * FROM meme";
+            $sqql = "SELECT * FROM meme where caption like %".$_GET['q']."%";
         } elseif (isset($_GET["m_id"])) {
             $sqql = "SELECT * FROM meme where Meme_id='" . $_GET["m_id"] . "'";
         } elseif (isset($_GET["m_id"])) {
